4edd65271a9c955f329dda719109c1624ead0604
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-10-05 14:21:40 +0530

    Create hello.js
    First lecture node.js 5-10-24

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+function hello() {
+  console.log("Hello From Function");
+}
+
+//Four Ways to Call a Function
+
+hello(); //1 :  simple Method
+hello.call(); //2 :  Call Method
+hello.apply(); //3 :  Apply Method
+hello``; //4 : Tilt sign method
