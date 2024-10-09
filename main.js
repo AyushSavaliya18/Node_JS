c9081d5fab2568b8d26b3d0531afe3509494cd7a
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-10-09 11:45:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,24 +14,3 @@ http
     res.end();
   })
   .listen(8000);
-
-// const http = require("http");
-// const { add, sub, multi, div } = require("./function");
-
-// http
-//   .createServer(function (req, res) {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-
-//     const n1 = 20;
-//     const n2 = 10;
-
-//     res.write(`Addition: ${add(n1, n2)}<br>`); // Use template literals and <br> for line breaks
-//     res.write(`Subtraction: ${sub(n1, n2)}<br>`);
-//     res.write(`Multiplication: ${multi(n1, n2)}<br>`);
-//     res.write(`Division: ${div(n1, n2)}`);
-
-//     res.end();
-//   })
-//   .listen(8000, () => {
-//     console.log("Server is running on port 8000"); // Add this line to log a success message
-//   });
