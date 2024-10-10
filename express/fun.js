a071b1439983f5e982c5e48bc745c924c5a94d59
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-10-10 15:11:43 +0530

    params function

--- express/fun.js
+++ express/fun.js
@@ -1,9 +1,22 @@
-let n1=3
-let n2 =6
 
 function add(n1, n2) {
     //   console.log("Sum is:", n1 + n2);
-    return "Sum is:" + (Number(n1) + Number(n2));
+    return "Sum is: " + (Number(n1) + Number(n2));
   
 }
-module.exports = {add};
+function sub(n1, n2) {
+    //   console.log("Sum is:", n1 + n2);
+    return "Sub is: " + (Number(n1) - Number(n2));
+  
+}
+function multi(n1, n2) {
+    //   console.log("Sum is:", n1 + n2);
+    return "multiplication is: " + (Number(n1) * Number(n2));
+  
+}
+function div(n1, n2) {
+    //   console.log("Sum is:", n1 + n2);
+    return "division is: " + (Number(n1) / Number(n2));
+  
+}
+module.exports = {add,sub,multi,div};
--- express/index.js
+++ express/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
-const {add} = require("./fun");
+const {add,sub,multi,div} = require("./fun");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send(add(5, 6));
+app.get("/add/:n1/:n2", function (req, res) {
+  // res.send(add(req.query.n1, req.query.n2));
+  res.send(add(req.params.n1, req.params.n2));
+});
+app.get("/sub/:n1/:n2", function (req, res) {
+  // res.send(sub(req.query.n1, req.query.n2));
+  res.send(sub(req.params.n1, req.params.n2));
 });
 
-app.listen(3000);
+app.get("/multi/:n1/:n2", function (req, res) {
+  // res.send(multi(req.query.n1, req.query.n2));
+  res.send(multi(req.params.n1, req.params.n2));
+});
+
+app.get("/div/:n1/:n2", function (req, res) {
+  // res.send(div(req.query.n1, req.query.n2));
+  res.send(div(req.params.n1, req.params.n2));
+});
+app.listen(8000);
