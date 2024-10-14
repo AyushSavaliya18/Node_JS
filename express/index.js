3982595a807fed54165c66b370d6fed0ce3586df
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-10-14 15:10:28 +0530

    CRUD

--- express/index.js
+++ express/index.js
@@ -1,3 +1,51 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
+
+const app = express();
+app.use(express.json());
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+async function getdata() {
+    await client.connect();
+    const db = client.db("employee");
+    const collection = db.collection("emp");
+
+    const data = await collection.find({}).toArray();
+
+    //   console.log(data);
+    return data;
+}
+// getdata();
+app.get("/emp", async function (req, res) {
+    const data = await getdata();
+    console.log(data);
+
+    res.send(data);
+});
+
+//*******************************   update
+
+async function insert(userData) {
+    await client.connect();
+    const db = client.db("employee");
+    const collection = db.collection("emp");
+
+    const data = await collection.insertOne(userData);
+    // console.log(data);
+    return data;
+}
+
+app.post("/insert", async function (req, res) {
+    const data = await insert(req.body);
+    // console.log(data);
+
+    res.send(data);
+});
+
+
+
+
+app.listen(8000);
