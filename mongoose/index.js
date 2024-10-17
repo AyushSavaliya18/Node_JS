8dd25e3633cc690d6079972af7035e7613550468
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-10-17 17:12:19 +0530

    mongoose packages

---
+++ mongoose/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/Ayush");
+const userschema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+const userModel = mongoose.model("user", userschema);
+app.post("/insert", async (req, res) => {
+  const userdata = {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+  };
+  const data = await userModel.create(userdata);
+  console.log(data);
+  res.send(data);
+});
+
+app.get("/getdata", async (req, res) => {
+  const data = await userModel.find();
+  res.send(data);
+});
+
+app.put("/update/:id", async (req, res) => {
+  const data = await userModel.updateOne(
+    {_id: req.params.id},
+    {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    }
+  );
+  console.log(data);
+  res.send(data);
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const data = await userModel.deleteOne({_id: req.params.id});
+  console.log(data);
+  res.send(data);
+});
+app.listen(2000);
