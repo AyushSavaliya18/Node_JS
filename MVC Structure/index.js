410bfdc13594cf931a83ab35ea4d9bac4ce019c2
AyushSavaliya18 <ayushsavaliya1111@gmail.com>
2024-12-05 11:00:31 +0530

    format code

--- MVC Structure/index.js
+++ MVC Structure/index.js
@@ -5,27 +5,28 @@ const privatekey = "#A*y*U*s*h#2710";
 const app = express(); 
 const bodyParser = require('body-parser')
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
-
 // app.use(express.json()) 
 
 const db = require("./database/db")
-//user functions
 
+//user functions
 const {verifytoken} = require("./middleware/userAuth");
 const {usercreate, userget, userupdate, userdelete,userlogin} = require("./controller/usercontroller")
 
-//send email api
+//Api For send email 
 const {sendEmail } = require("./controller/mail")
 app.get("/sendEmail",sendEmail);
 
-//otp match api
+//API For otp match 
 const {submitotp,resetpassword} = require("./controller/FPass")
 app.post("/submitotp",submitotp);
 app.post("/reset",resetpassword);
 
+//User Login
+app.post("/login", userlogin);
 
 //user API
 app.post("/insertuser", usercreate);
@@ -33,15 +34,12 @@ app.get("/getuser", verifytoken,userget);
 app.put("/updateuser/:id", userupdate)
 app.delete("/deleteuser/:id", userdelete);
 
-//User Login
-app.post("/login", userlogin);
 
 //Category Functions
 const { createcategory } = require("./controller/categorycontroller")
 const { getcategory } = require("./controller/categorycontroller")
 const { updatecategory } = require("./controller/categorycontroller")
 const { deletecategory } = require("./controller/categorycontroller")
-
 
 //Category API
 app.post("/createcategory", createcategory);
